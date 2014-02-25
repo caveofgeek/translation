6f8d394eef51dbaa183c63bfb382fd6a159dfb90
Tanut Viteevanich <aon@addresstalk.com>
2014-02-26 02:53:39 +0700

    Remove unused items.

--- translate.php
+++ translate.php
@@ -1,34 +1,36 @@
+<meta charset="utf8">
 <?php
-  require("GTranslate.php");
-  error_reporting(E_ALL);
-  ini_set('display_error', 1);
+  function getSource($url){
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+  }
+
+  function replace_unicode_escape_sequence($match) {
+    return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
+  }
 
   if(isset($_POST['submit'])) {
     $contents = file_get_contents($_POST['input_file']);
+    $contents = str_replace(array('\n','\r\n',PHP_EOL),array('','',''), $contents);
+    $contents = urlencode($contents);
+    $from = $_POST['from'];
+    $to = $_POST['to'];
 
-    try {
-      # $apikey = 'AIzaSyClJOuVy061SG6Mwvc7mSmqBJtbkab8Tlo';
-      $gt = new Gtranslate();
-      # $gt->setApiKey($apikey);
+    $url = "http://translate.google.com/translate_a/t?";
+    $url .= "client=t&q=$contents&hl=$to&sl=$from&tl=$to";
+    $url .= "&ie=UTF-8&oe=UTF-8&multires=1&otf=1&pc=1&trs=1&ssel=3&tsel=6&sc=1";
 
-      if($_POST['from'] == "th" && $_POST['to'] == "en")
-        $translated =  $gt->thai_to_english($contents);
-      elseif($_POST['from'] == "th" && $_POST['to'] == "vi")
-        $translated =  $gt->thai_to_vietnamse($contents);
-      elseif($_POST['from'] == "en" && $_POST['to'] == "th")
-        $translated =  $gt->english_to_thai($contents);
-      elseif($_POST['from'] == "en" && $_POST['to'] == "vi")
-        $translated =  $gt->english_to_vietnamse($contents);
-      elseif($_POST['from'] == "vi" && $_POST['to'] == "th")
-        $translated =  $gt->vietnamse_to_thai($contents);
-      elseif($_POST['from'] == "vi" && $_POST['to'] == "en")
-        $translated =  $gt->vietnamse_to_en($contents);
-    }
-    catch (GTranslateException $ge) { echo $ge->getMessage(); }
+    $translated = getSource($url);
+    preg_match('/\[{3}"(.+?) "/', $translated, $translated);
+    $translated = preg_replace_callback('/\\\\u([0-9a-f]{4})/i', 'replace_unicode_escape_sequence', $translated[1]);
+    $translated = preg_replace('/< -.+?- >/s', '', $translated);
   }
 ?>
 
-<meta charset="utf8">
 <html>
   <head>
     <style>
@@ -39,34 +41,41 @@
         background-color: #aaa;
       }
 
-      form.container {
+      div.container {
         padding: 30px;
         margin: 0 auto;
         text-align: center;
+      }
+
+      textarea {
+        width: 90%;
+        margin: 0 auto;
       }
     </style>
   </head>
   <body>
     <div class="wrapper">
-      <form action="#" method="post" class="container">
-        <input type="file" name="input_file" id="input_file" required/>
-        <p>
-          <select name="from">
-            <option value="th">ไทย</option>
-            <option value="en">อังกฤษ</option>
-            <option value="vi">เวียดนาม</option>
-          </select>
-          To
-          <select name="to">
-            <option value="th">ไทย</option>
-            <option value="en">อังกฤษ</option>
-            <option value="vi">เวียดนาม</option>
-          </select>
-        </p>
-        <input type="submit" name="submit" value="Translate">
-      </form>
-      <div class="result">
-        <p><? echo $translated; ?></p>
+      <div class="container">
+        <form action="#" method="post">
+          <input type="file" name="input_file" id="input_file"/>
+          <p>
+            <select name="from">
+              <option value="th">ไทย</option>
+              <option value="en">อังกฤษ</option>
+              <option value="vi">เวียดนาม</option>
+            </select>
+            To
+            <select name="to">
+              <option value="th">ไทย</option>
+              <option value="en">อังกฤษ</option>
+              <option value="vi">เวียดนาม</option>
+            </select>
+          </p>
+          <input type="submit" name="submit" value="Translate">
+        </form>
+        <div class="result">
+          <p><textarea rows="5"><? echo $translated; ?></textarea></p>
+        </div>
       </div>
     </div>
   </body>
