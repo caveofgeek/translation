2836be81b206d3b751f317895ee5b1626deb878a
Tanut Viteevanich <aon@addresstalk.com>
2014-02-26 15:53:44 +0700

    Translate whole file with google.

--- translate.php
+++ translate.php
@@ -14,20 +14,36 @@
   }
 
   if(isset($_POST['submit'])) {
-    $contents = file_get_contents($_POST['input_file']);
+    $contents = file_get_contents($_FILES["source"]["tmp_name"]);
+    $contents = preg_replace('/<!--.+?-->/s', '', $contents);
     $contents = str_replace(array('\n','\r\n',PHP_EOL),array('','',''), $contents);
     $contents = urlencode($contents);
+    $contents = str_split($contents, 100);
     $from = $_POST['from'];
     $to = $_POST['to'];
 
-    $url = "http://translate.google.com/translate_a/t?";
-    $url .= "client=t&q=$contents&hl=$to&sl=$from&tl=$to";
-    $url .= "&ie=UTF-8&oe=UTF-8&multires=1&otf=1&pc=1&trs=1&ssel=3&tsel=6&sc=1";
+    /*
+    $doc = new DOMDocument();
+    $doc->loadHTMLFile('test.html');
+    $divs = $doc->getElementsByTagName('div');
+    foreach($divs as $n) {
+        echo $n->nodeValue;
+    }
+    */
 
-    $translated = getSource($url);
-    preg_match('/\[{3}"(.+?) "/', $translated, $translated);
-    $translated = preg_replace_callback('/\\\\u([0-9a-f]{4})/i', 'replace_unicode_escape_sequence', $translated[1]);
-    $translated = preg_replace('/< -.+?- >/s', '', $translated);
+    foreach ($contents as $value) {
+      $url = "http://translate.google.com/translate_a/t?";
+      $url .= "client=t&q=$value&hl=$to&sl=$from&tl=$to";
+      $url .= "&ie=UTF-8&oe=UTF-8&multires=1&otf=1&pc=1&trs=1&ssel=3&tsel=6&sc=1";
+
+      $translated = getSource($url);
+      preg_match('/\[{3}"(.+?)",/', $translated, $translated);
+      $translated = str_replace("\\\"", "\"", rtrim($translated[1], "."));
+
+      $whole_word .= $translated;
+    }
+
+    $translated = preg_replace_callback('/\\\\u([0-9a-f]{4})/i', 'replace_unicode_escape_sequence', $whole_word);
   }
 ?>
 
@@ -56,8 +72,8 @@
   <body>
     <div class="wrapper">
       <div class="container">
-        <form action="#" method="post">
-          <input type="file" name="input_file" id="input_file"/>
+        <form action="#" method="post" enctype="multipart/form-data">
+          <input type="file" name="source" required>
           <p>
             <select name="from">
               <option value="th">ไทย</option>
