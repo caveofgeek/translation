dad9baa68fd1feedd3743716725a8fe24de4b72c
Tanut Viteevanich <aon@addresstalk.com>
2014-03-03 17:58:28 +0700

    Output file

--- translate.php
+++ translate.php
@@ -3,13 +3,22 @@
   error_reporting(E_ALL);
   ini_set('display_error', 1);
 
-  $contents = file_get_contents($_FILES["source_file"]["tmp_name"]);
-  $contents = str_split($contents, 800);
-  $source = $_POST['source'];
-  $target = $_POST['target'];
+  if(isset($_GET['file'])){
+    $contents = file_get_contents($_GET['file']);
+    $contents = str_split($contents, 800);
+    $source = $_GET['source'];
+    $target = $_GET['target'];
+    $output = $_GET['output'];
+  }else {
+    $contents = file_get_contents($_FILES["source_file"]["tmp_name"]);
+    $contents = str_split($contents, 800);
+    $source = $_POST['source'];
+    $target = $_POST['target'];
+  }
 
   try {
     $apikey = 'AIzaSyClJOuVy061SG6Mwvc7mSmqBJtbkab8Tlo';
+    //$apikey = 'PUT YOU API KEY';
     $gt = new Gtranslate();
     $gt->setApiKey($apikey);
     //$gt->setRequestType('curl');
@@ -23,7 +32,13 @@
       $value = substr($value, 0, $i);
       $translated .= $gt->{$source."_to_".$target}($value);
     }
-    echo $translated;
+
+    if(isset($_GET['file'])){
+
+      $file = "output/$output";
+      file_put_contents($file, "<meta charset='utf8'>$translated");
+      echo htmlentities($translated, "UTF-8");
+    }else echo $translated;
   }
   catch (GTranslateException $ge) { echo $ge->getMessage(); }
 ?>
